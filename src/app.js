3ef8ac057ad1685726e4fbd7cee4d200a1a0430e
Sebastian <seav22@gmail.com>
2024-02-21 21:03:25 +0100

    added CORS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const db = require("./db/db.js");
 const router = require("./routes/task.js");
 
 const Port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/todolist", router);
 
